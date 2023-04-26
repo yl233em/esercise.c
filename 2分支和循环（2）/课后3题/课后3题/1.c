4c8cc270eefb2751d716633d6013c64d4c1c2e23
yl233em <3156586673@qq.com>
2023-04-26 21:38:13 +0800

    3题

---
+++ 2分支和循环（2）/课后3题/课后3题/1.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int tem = 0;
+
+	scanf("%d %d", &a, &b);
+	while ()
+	{
+		if(a >= b)
+		{
+			tem = a % b;
+		}
+	}
+
+	printf("这两个数的最大公约数是%d", b);
+
+	return 0;
+}
