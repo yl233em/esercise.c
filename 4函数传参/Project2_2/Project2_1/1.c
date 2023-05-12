dde935ff79dfe91523304e560ef7831822d8aa82
yl233em <3156586673@qq.com>
2023-05-12 22:49:26 +0800

    非递归实现求阶乘

---
+++ 4函数传参/Project2_2/Project2_1/1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//递归和非递归分别实现求n的阶乘（不考虑溢出的问题）
+
+#include<stdio.h>
+
+int main()
+{
+	int i = 0;
+	int n = 0;
+	int ret = 1;
+	printf("输入数字：");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		ret *= i;
+	}
+	printf("该数阶乘：%d", ret);
+
+	return 0;
+}
