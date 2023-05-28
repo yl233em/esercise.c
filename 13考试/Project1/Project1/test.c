727046f8da6fa5fee4e24e9914e093bdb57b3636
yl233em <3156586673@qq.com>
2023-05-28 23:18:50 +0800

    倒置字符串

--- 13考试/Project1/Project1/test.c
+++ 13考试/Project1/Project1/test.c
@@ -2,22 +2,40 @@
 //µ¹ÖÃ×Ö·û´®
 #include<stdio.h>
 #include<string.h>
+void re(char* l, char* r)
+{
+	char tem = 0;
+	while (l < r)
+	{
+		tem = *l;
+		*l = *r;
+		*r = tem;
+		l++;
+		r--;
+	}
+}
 int main()
 {
 	int i = 0, j = 0;
-	char temp = 0;
 	char c[100] = { 0 };
 	gets(c);
 	int len = strlen(c);
-
 	char* left = c;
-	char* right = &c[len];
-	/*for (i = 0, j = len-1; i < len; i++, j--)
+	char* right = c + len - 1;
+	char* cut = c;
+	re(left, right);  
+	while (*cut != '\0')
 	{
-		temp = c[i];
-		c[i] = c[j];
-		c[j] = temp;
-	}
-	printf("%s", c);*/
+		while (*cut != ' ' && *cut != '\0')
+		{
+			cut++;
+		}
+		right = cut - 1;
+		re(left, right);
+		left = cut + 1;
+		cut++;
+ 	}
+	printf("%s", c);
+
 	return 0;
 }
